56e406f73488fe910dc4ab487281d02b6f7edb25
Florian Waibel <fwaibel@eclipsesource.com>
2014-12-17 21:11:48 +0100

    Adds tomcat-server to Gradle build

---
+++ org.eclipse.virgo.web.core/src/main/java/org/eclipse/virgo/web/core/internal/BundlorGradlePluginDoesntLookIntoXmlFiles.java
@@ -0,0 +1,8 @@
+package org.eclipse.virgo.web.core.internal;
+
+import org.eclipse.virgo.kernel.osgi.framework.OsgiFramework;
+
+public class BundlorGradlePluginDoesntLookIntoXmlFiles {
+
+    OsgiFramework agentDMStartTracker;
+}
